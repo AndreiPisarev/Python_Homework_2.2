8da0c3299efe768705654af4502ae27e239aa84b
AndreiPisarev <berky.87@gmail.com>
2018-04-07 20:38:43 +0300

    Определение кодировки и чтение файла за одни вызов open

--- read_files.py
+++ read_files.py
@@ -1,8 +1,4 @@
 """
-def detect_encording(name_file) - читаем файл побайтно, определяет кодировку файла и возвращает её
-
-def detect_encording(name_file) - принимает кодировку, читает файл построчно удаляя смволы \n и разделяе строку на слова
-возвращает список слов длинее 6 символов в файле
 
 def top_10_popular(list_char_more_6) - принимает список из слов длинее 6 символов, находит число повторений каждого
 слова, формирует вложенный список вида [[число повторений, слово], [число повторений, слово]].Убирает повтор. элекменты,
@@ -12,28 +8,29 @@ def top_10_popular(list_char_more_6) - принимает список из сл
 print_word(word, name_file) - принимает список топ 10 и имя обрабатываемого файла, формирует печать
 
 main() - указывает список файлов для обработки, запускаем в цикле обработку каждого файла:
-- определяем кодировку
-- читаем файл определенной кодировкой
-- назодим топ 10 слов
+
+- читаем файл, получаем список символов длинее 6 символов.
+- находим топ 10 слов
 - выводим информацию
 """
 
 import chardet
 
 
-def detect_encording(name_file):
-    with open(name_file, 'rb') as f:
-        data = f.read()
+def read_file(name_file):
+
+    """читаем файл побайтно, определяет кодировку файла, декодируем, удаляем смволы \n и разделяе строку на слова
+    возвращает список слов длинее 6 символов в файле"""
+
+    with open(name_file, 'rb') as news_file:
+        data = news_file.read()
         result = chardet.detect(data)
-        print("\nКодировка в {}: {}".format(name_file, result['encoding']))
+        print("\nКодировка в {}: {}".format(name_file, result[ 'encoding' ]))
 
-        return result['encoding']
+        data_decode = data.decode(result['encoding'])
 
-
-def read_file(name_file, encoding_file):
-    with open(name_file, encoding=encoding_file) as news_file:
         list_char_more_6 = list()
-        string_file = news_file.read().replace('\n', '')  # Подкинули задачку с \n  в начале строки :)
+        string_file = data_decode.replace('\n', '')  # Подкинули задачку с \n  в начале строки :)
         list_string_file = string_file.lower().split(' ')
         list_char_more_6 = [i for i in list_string_file if len(i) > 6]  # Наконец научился так писать
 
@@ -41,9 +38,11 @@ def read_file(name_file, encoding_file):
 
 
 def top_10_popular(list_char_more_6):
+    """ Принимаем список слов длинее 6 символов, проходим по списку и составляем словарь {слово: повторения}
+    сортируем сло"""
     list_word = list()
-
     freq = {}
+
     for word in list_char_more_6:
         if word not in freq:
             freq[word] = 1
@@ -70,8 +69,8 @@ def main():
     list_file = ['newsafr.txt', 'newscy.txt', 'newsfr.txt', 'newsit.txt']
 
     for name_file in list_file:
-        encoding_file = detect_encording(name_file)
-        list_char_more_6 = read_file(name_file, encoding_file)
+        # encoding_file = detect_encording(name_file)
+        list_char_more_6 = read_file(name_file)
         list_word = top_10_popular(list_char_more_6)
         print_word(list_word, name_file)
 
